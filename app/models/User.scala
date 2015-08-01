aabee60107bba2b6731d58dd121f5d17feede75b
Andres Taylor <andres@neotechnology.com>
2015-08-01 12:28:18 +0200

    Restarting

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,12 +1,25 @@
 package models
 
-import org.mindrot.jbcrypt.BCrypt
+import java.util.UUID
 
-object User {
-  def createUser(name: String, email: String, password: String) =
-    new User(name, email, passwordHash = BCrypt.hashpw(password, BCrypt.gensalt()))
-}
+import com.mohiva.play.silhouette.api.{ Identity, LoginInfo }
 
-case class User(name: String, email: String, passwordHash: String) {
-  def checkPassword(plainText: String) = BCrypt.checkpw(plainText, passwordHash)
-}
+/**
+ * The user object.
+ *
+ * @param userID The unique ID of the user.
+ * @param loginInfo The linked login info.
+ * @param firstName Maybe the first name of the authenticated user.
+ * @param lastName Maybe the last name of the authenticated user.
+ * @param fullName Maybe the full name of the authenticated user.
+ * @param email Maybe the email of the authenticated provider.
+ * @param avatarURL Maybe the avatar URL of the authenticated provider.
+ */
+case class User(
+  userID: UUID,
+  loginInfo: LoginInfo,
+  firstName: Option[String],
+  lastName: Option[String],
+  fullName: Option[String],
+  email: Option[String],
+  avatarURL: Option[String]) extends Identity
